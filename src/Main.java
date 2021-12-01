5e79468837322b0f9e45c764a3a2624f529f5cbc
Igor Mandrik <creation_86@mail.ru>
2021-12-01 15:15:17 +0800

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceTicket = 2000;
+        int priceBonusMile = 20;
+        int bonus = priceTicket / priceBonusMile;
+        System.out.println("Вам начислено: " + bonus + " миль");
+    }
+}
